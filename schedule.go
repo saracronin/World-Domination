3cb28f910f4adfa1dd30a78c374330a56b15b9bf
Matthew Turnshek <matthewturnshek@gmail.com>
2018-11-16 21:29:07 -0500

    update schedule

--- schedule.go
+++ schedule.go
@@ -2,4 +2,5 @@ package main
 import "fmt"
 func main() {
     fmt.Printf("11.15.18 - New laptop is scheduled to arrive.\nLet the shenanigans commence!")
+    fmt.Printf("11.23.18 - 9:21 PM - Matthew arrives at MCO.\n")
 }
