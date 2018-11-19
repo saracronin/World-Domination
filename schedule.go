efceb1785f3fdb9ec4bbf434937df9f8d4945b62
Sarah Bednar <sariebeary@sarahs-mbp.home>
2018-11-18 22:35:47 -0500

    Fixed format

--- schedule.go
+++ schedule.go
@@ -1,7 +1,7 @@
 package main
 import "fmt"
 func main() {
-    fmt.Printf("11.15.18 - New laptop is scheduled to arrive.\nLet the shenanigans commence!")
+    fmt.Printf("11.15.18 - New laptop is scheduled to arrive. Let the shenanigans commence!\n")
     fmt.Printf("11.23.18 - 21:21 - Matthew arrives at MCO.\n")
     fmt.Printf("11.26.18 - 06:50 - Matthew departs from MCO.\n")
 }
